95c300d76334912ed9bc80590e6bbc795a377f54
Lea Arcidiacono <live2run_1980@yahoo.com>
2024-04-20 08:01:17 -0400

    searchbook jsx

--- client/.eslintrc.cjs
+++ client/.eslintrc.cjs
@@ -1,3 +1,4 @@
+
 module.exports = {
   env: { browser: true, es2020: true },
   extends: [
@@ -11,5 +12,7 @@ module.exports = {
   plugins: ['react-refresh'],
   rules: {
     'react-refresh/only-export-components': 'warn',
+    'no-unused-vars': 'warn',
+    'no-undef': 'warn',
   },
 }
--- client/src/pages/SearchBooks.jsx
+++ client/src/pages/SearchBooks.jsx
@@ -1,4 +1,7 @@
 import { useState, useEffect } from 'react';
+import { useMutation } from '@apollo/client';
+import { SAVE_BOOK } from '../utils/mutations';
+
 import {
   Container,
   Col,
@@ -26,6 +29,8 @@ const SearchBooks = () => {
   useEffect(() => {
     return () => saveBookIds(savedBookIds);
   });
+
+  const [saveBookMutation, {error}] = useMutation(SAVE_BOOK);
 
   // create method to search for books and set state on form submit
   const handleFormSubmit = async (event) => {
@@ -72,11 +77,13 @@ const SearchBooks = () => {
     }
 
     try {
-      const response = await saveBook(bookToSave, token);
+      const { data } = await saveBookMutation({ variables: { input: bookToSave } });
 
-      if (!response.ok) {
-        throw new Error('something went wrong!');
-      }
+     //const response = await saveBook(bookToSave, token);
+
+      // if (!response.ok) {
+      //   throw new Error('something went wrong!');
+      // }
 
       // if book successfully saves to user's account, save book id to state
       setSavedBookIds([...savedBookIds, bookToSave.bookId]);
@@ -152,3 +159,4 @@ const SearchBooks = () => {
 };
 
 export default SearchBooks;
+
